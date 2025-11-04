873e4598aadde32b0d3a3a39aba4c6d0d074fe9d
Lin18210 <84231180+Lin18210@users.noreply.github.com>
2025-11-04 22:17:25 +0700

    ..

--- Cafe/src/pages/Contact.jsx
+++ Cafe/src/pages/Contact.jsx
@@ -96,7 +96,7 @@ const Contact = () => {
               
               <div className="mb-8">
                 <h3 className="font-semibold text-amber-200 mb-2">Email Us</h3>
-                <p>cafelight@gmail.com</p>
+                <p>cafedelight@gmail.com</p>
               </div>
               
               <div>
